52bb27fc719baf8bc410c08f0ffb8f17d4623358
DanielCH5 <163397248+DanielCH5@users.noreply.github.com.>
2025-05-19 23:54:34 +0200

    exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let sum = 0;
+    let isInteger = Number.isInteger(first, last);
+   
+    if (first < 0 || last < 0
+        ||  !isInteger || typeof first != "number" || typeof last != "number"
+    ){
+        return "ERROR";
+    } else if (last < first){
+        for(i = last; i <= first; i++){
+            sum += i;
+        }
+        return sum;
+    } else{
+        for (i = first; i <= last; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
