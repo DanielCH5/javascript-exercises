fa0c3b668c60794235fcb2c1a50ef785ecfa5729
DanielCH5 <163397248+DanielCH5@users.noreply.github.com>
2025-05-20 17:10:29 +0200

    Exercise 10 and 11 completed

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //Checks if the year is divisible with 4 AND 400 to
+    //check for leapyear
+    if (year % 4 === 0 && year % 400 === 0){
+        return true;
+    }
+    //Then checks if the year is divisible with 4 and
+    //100
+     else if (year % 4 === 0 && year % 100 === 0){
+        return false;
+    } //Checks if divisible with 4 in general
+    else if (year % 4 === 0){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //Convert to celcius using the formula
+  const celcius = (fahrenheit - 32) * 5 / 9;
+  //Round the number up to one decimal
+  const rounded = Math.round(celcius * 10) / 10;
+  return rounded;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+
+  //Same as conversion to celcius but other way around
+  const fahrenheit = celcius * (9 / 5) + 32;
+  
+  const rounded = Math.round(fahrenheit * 10) / 10;
+
+  return rounded;
 };
 
 // Do not edit below this line
